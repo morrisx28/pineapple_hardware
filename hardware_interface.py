2798bb6a24ea7fcb2dd749860e4d76e2ba567ede
morris <morrisx28@gmail.com>
2025-12-04 14:35:46 +0800

    integrate multi imu interface with hardware interface

--- hardware_interface.py
+++ hardware_interface.py
@@ -7,6 +7,7 @@ import usb.util
 
 from damiao import *
 from xsens_imu_test import *
+from multi_imu.lib import pineapple_multi_imu
 
 from csl_sdk2py.core.channel import ChannelPublisher, ChannelFactoryInitialize
 from csl_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
@@ -25,6 +26,7 @@ DEVICE_ID = "8618B478D60114596A72552FA077E579"
 MOTOR_ID_LIST = [(DM_Motor_Type.DM8006, 0x01, 0x11), (DM_Motor_Type.DM8006, 0x03, 0x13), 
                  (DM_Motor_Type.DM8009, 0x05, 0x15), (DM_Motor_Type.DM8009, 0x06, 0x16)]
 NUM_MOTOR = 4
+NUM_IMU = 3
 
 class DDSHandler:
     def __init__(self, domain_id=1, interface="lo"):
@@ -32,17 +34,22 @@ class DDSHandler:
 
 
         self.motor_list = list()
-        self.is_running = False
+        self.imu_list = list()
+        self.is_running = True
         self.dt = 0.001
+        self.imu_dt = 0.002
 
         self.init_motor_manager()
-        self.body_imu = XsensIMU()
+        self.init_multi_imu()
+        # self.body_imu = XsensIMU()
 
         self.low_state_publisher = ChannelPublisher(TOPIC_LOWSTATE, LowState_)
         self.low_state_publisher.Init()
         self.low_state = csl_pineapple_msg_dds__LowState_()
         self.low_state_pub_thread = threading.Thread(target=self.low_state_handler, daemon=True)
         self.low_state_pub_thread.start()
+        self.multi_imu_thread = threading.Thread(target=self.process_multi_imu, daemon=True)
+        self.multi_imu_thread.start()
 
         self.low_cmd_subscriber = ChannelSubscriber(TOPIC_LOWCMD, LowCmd_)
         self.low_cmd_subscriber.Init(self.low_cmd_handler, 10)
@@ -55,9 +62,36 @@ class DDSHandler:
                     can_id=can_id,
                     mst_id=mst_id))
         
-        self.motor_manager = Motor_Control(1000000, 5000000,DEVICE_ID,self.motor_list) 
+        self.motor_manager = Motor_Control(1000000, 5000000,DEVICE_ID,self.motor_list)  
+
+    def init_multi_imu(self):
+        for i in range(NUM_IMU):
+            imu = pineapple_multi_imu.ImuRs485Worker(f"/dev/ttyUSB{i}")
+            imu.start()
+            self.imu_list.append(imu)
+        print("Wait for active multi IMU sensor")
+        time.sleep(1.0)
+        print("Multi IMU sensor active")
     
-    
+    def process_multi_imu(self):
+        while self.is_running:
+            for i in range(NUM_IMU):
+                step_start = time.perf_counter()
+                imu_data = self.imu_list[i].snapshot()
+                # print(f"quat{i}:, {imu_data.quaternion}")
+                # print(f"rpy{i}:, {imu_data.rpy}")
+                # print(f"gyro{i}:, {imu_data.gyro}")
+                # print(f"accel{i}:, {imu_data.accel}")
+                for j in range(4):
+                   self.low_state.imu_state[i].quaternion[j] = imu_data.quaternion[j]
+                
+                for j in range(3):
+                   self.low_state.imu_state[i].gyroscope[j] = imu_data.gyro[j]
+
+            time_until_next_step = self.imu_dt - (time.perf_counter() - step_start)
+            if time_until_next_step > 0:
+                time.sleep(time_until_next_step)
+
     def low_state_handler(self):
         while True:
             step_start = time.perf_counter()
@@ -69,13 +103,13 @@ class DDSHandler:
                 # print(f"canid is: {self.motor_list[i]} pos: {self.low_state.motor_state[i].q} vel: {self.low_state.motor_state[i].dq} effort: {self.low_state.motor_state[i].tau_est} ", file=sys.stderr)
 
             # pub body imu quatanion data
-            quaternion = self.body_imu.get_quaternion()
-            angular_vel = self.body_imu.get_angular_vel()
-            for i in range(4):
-                self.low_state.imu_state.quaternion[i] = quaternion[i]
+            # quaternion = self.body_imu.get_quaternion()
+            # angular_vel = self.body_imu.get_angular_vel()
+            # for i in range(4):
+            #    self.low_state.imu_state.quaternion[i] = quaternion[i]
             
-            for i in range(3):
-                self.low_state.imu_state.gyroscope[i] = angular_vel[i]
+            # for i in range(3):
+            #    self.low_state.imu_state.gyroscope[i] = angular_vel[i]
 
             self.low_state_publisher.Write(self.low_state)
             time_until_next_step = self.dt - (time.perf_counter() - step_start)
@@ -90,16 +124,22 @@ class DDSHandler:
                 self.motor_manager.control_mit(self.motor_manager.getMotor(self.motor_list[i].can_id), 
                                                self.low_cmd.motor_cmd[i].kp, self.low_cmd.motor_cmd[i].kd,
                                                self.low_cmd.motor_cmd[i].q, self.low_cmd.motor_cmd[i].dq, self.low_cmd.motor_cmd[i].tau)
-
+            # print("low cmd dq: ", self.low_cmd.motor_cmd[0].dq)
     
     def close(self):
         self.is_running = False
         self.motor_manager.close()
+        for imu in self.imu_list:
+            imu.stop()
 
 
 if __name__ == '__main__':
     
-    hardware_manager = DDSHandler()
+    if len(sys.argv)>1:
+        hardware_manager = DDSHandler(1, sys.argv[1])
+    else:
+        hardware_manager = DDSHandler()
+    
     command_dict = {
         # "test": hardware_manager.test,
     }
--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import traceback
 import yaml
 import csv
 import argparse
+import matplotlib.pyplot as plt # Import for plotting
 
 from csl_sdk2py.core.channel import ChannelPublisher, ChannelFactoryInitialize
 from csl_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
@@ -36,13 +37,21 @@ class Controller:
         self.low_cmd = csl_pineapple_msg_dds__LowCmd_()  
         self.low_state = None  
 
-
         self.controller_rt = 0.0
         self.is_running = False
 
         self.ang_vel_data = []
         self.qtau_data = []
         self.qtau_cmd = []
+
+        # Data logging lists for plotting (for motor 0 as an example)
+        self.time_data = []
+        self.qpos_data = []
+        self.qvel_data = []
+        self.qtau_data = []
+        self.dq_cmd_data = []
+        self.tau_cmd_data = []
+
 
         # thread handling
         self.lowCmdWriteThreadPtr = None
@@ -57,6 +66,7 @@ class Controller:
 
         self.mode = ''
         self.dt = 0.001
+        self.start_time = time.perf_counter() # To calculate elapsed time
 
 
         self.crc = CRC()
@@ -75,6 +85,7 @@ class Controller:
 
         # Init default pos #
         self.Start()
+        self.start_time = time.perf_counter() # Reset start time after threads are initialized
 
         print("Initial Sucess !!!")
 
@@ -100,6 +111,8 @@ class Controller:
 
     def ShutDown(self):
         self.is_running = False
+        if self.lowCmdWriteThreadPtr:
+            self.lowCmdWriteThreadPtr.join()
 
 
     # Private methods
@@ -119,6 +132,7 @@ class Controller:
     def LowStateMessageHandler(self, msg: LowState_):
         self.low_state = msg
         self.get_current_state()
+        self.record_data() # Record data when a new state message is received
         # print(f'qpos {self.low_state.motor_state[0].q}')
         # quat = self.low_state.imu_state.quaternion
         # ang_vel = self.low_state.imu_state.gyroscope
@@ -130,7 +144,15 @@ class Controller:
         for i in range(NUM_MOTORS):
             self.low_cmd.motor_cmd[i].q = 0
             self.low_cmd.motor_cmd[i].kp = 0
-            self.low_cmd.motor_cmd[i].dq = 1.0
+            self.low_cmd.motor_cmd[i].dq = 1.0 # Command velocity of 1.0
+            self.low_cmd.motor_cmd[i].kd = 1.0
+            self.low_cmd.motor_cmd[i].tau = 0.0
+    
+    def test1(self):
+        for i in range(NUM_MOTORS):
+            self.low_cmd.motor_cmd[i].q = 0
+            self.low_cmd.motor_cmd[i].kp = 0
+            self.low_cmd.motor_cmd[i].dq = -1.0 # Command velocity of -1.0
             self.low_cmd.motor_cmd[i].kd = 1.0
             self.low_cmd.motor_cmd[i].tau = 0.0
             
@@ -138,6 +160,11 @@ class Controller:
 
     def st_test(self):
         self.mode = 'test'
+        print("Mode set to 'test': Target motor velocity 1.0")
+    
+    def st_test1(self):
+        self.mode = 'test1'
+        print("Mode set to 'test1': Target motor velocity -1.0")
 
 
     def get_current_state(self):
@@ -147,11 +174,64 @@ class Controller:
             self.qtau[i] = self.low_state.motor_state[i].tau_est
         
         for i in range(3):
-            self.ang_vel[i] = self.low_state.imu_state.gyroscope[i]
+            self.ang_vel[i] = self.low_state.imu_state[0].gyroscope[i]
+        
+        # print("angular vel: ", self.ang_vel)
 
         for i in range(4):
-            self.quat[i] = self.low_state.imu_state.quaternion[i]
+            self.quat[i] = self.low_state.imu_state[0].quaternion[i]
     
+
+    def record_data(self):
+        """Records current state and command data for plotting."""
+        # Use motor 0 for plotting as an example
+        motor_idx = 0 
+        
+        current_time = time.perf_counter() - self.start_time
+        
+        self.time_data.append(current_time)
+        self.qpos_data.append(self.qpos[motor_idx])
+        self.qvel_data.append(self.qvel[motor_idx])
+        self.qtau_data.append(self.qtau[motor_idx])
+        
+        # Record command for the active motor
+        self.dq_cmd_data.append(self.low_cmd.motor_cmd[motor_idx].dq)
+        self.tau_cmd_data.append(self.low_cmd.motor_cmd[motor_idx].tau)
+
+    def plot_data(self):
+        """Plots the recorded state and command data."""
+        if not self.time_data:
+            print("No data recorded to plot.")
+            return
+
+        print("Generating plots...")
+
+        fig, axs = plt.subplots(3, 1, figsize=(10, 8), sharex=True)
+        fig.suptitle('Motor 0 State and Command Over Time')
+
+        # 1. Position Plot
+        axs[0].plot(self.time_data, self.qpos_data, label='Actual Position (q)')
+        axs[0].set_ylabel('Position (rad)')
+        axs[0].grid(True)
+        axs[0].legend()
+
+        # 2. Velocity Plot (Actual vs Command)
+        axs[1].plot(self.time_data, self.qvel_data, label='Actual Velocity (dq)', color='blue')
+        axs[1].plot(self.time_data, self.dq_cmd_data, label='Command Velocity (dq_cmd)', linestyle='--', color='orange')
+        axs[1].set_ylabel('Velocity (rad/s)')
+        axs[1].grid(True)
+        axs[1].legend()
+
+        # 3. Torque Plot (Estimated vs Command)
+        axs[2].plot(self.time_data, self.qtau_data, label='Estimated Torque (tau_est)', color='green')
+        axs[2].plot(self.time_data, self.tau_cmd_data, label='Command Torque (tau_cmd)', linestyle=':', color='red')
+        axs[2].set_ylabel('Torque (Nm)')
+        axs[2].set_xlabel('Time (s)')
+        axs[2].grid(True)
+        axs[2].legend()
+
+        plt.tight_layout(rect=[0, 0, 1, 0.96]) # Adjust layout to fit suptitle
+        plt.show()
 
 
     def LowCmdWrite(self):
@@ -160,6 +240,9 @@ class Controller:
             step_start = time.perf_counter()
             if self.mode == 'test':
                 self.test()
+            elif self.mode == 'test1':
+                self.test1()
+            
             self.low_cmd.crc = self.crc.Crc(self.low_cmd)
             self.lowcmd_publisher.Write(self.low_cmd)
 
@@ -189,6 +272,7 @@ if __name__ == '__main__':
 
     command_dict = {
         "test": controller.st_test,
+        "test1": controller.st_test1,
     }
 
     while True:        
@@ -196,6 +280,8 @@ if __name__ == '__main__':
             cmd = input("CMD :")
             if cmd in command_dict:
                 command_dict[cmd]()
+            elif cmd == "plot":
+                controller.plot_data() # New command to plot
             elif cmd == "exit":
                 controller.ShutDown()
                 break
@@ -203,4 +289,4 @@ if __name__ == '__main__':
         except Exception as e:
             traceback.print_exc()
             break
-    sys.exit(-1)   
+    sys.exit(-1)
